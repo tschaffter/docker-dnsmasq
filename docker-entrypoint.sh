ab78509f78cf21780b067225adf26fdd2bccc9df
tschaffter <thomas.schaffter@gmail.com>
2021-03-22 10:48:57 -0700

    Minor udpate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 
 if [ "$1" = 'dnsmasq' ]; then
     if [[ ! -z "${SERVER_HOST}" && ! -z "${SERVER_PORT}" ]]; then
-        # Lookup IP of server container as work around because dnsmasq cannot
-        # resolve the address from the hostname "server". This uses ping rather
-        # than 'dig +short server' to avoid needing dnsutils package.
+        # Lookup IP of nameserver container as work around because dnsmasq
+        # cannot resolve the address from the nameserver "server". This uses
+        # ping rather than 'dig +short server' to avoid needing dnsutils
+        # package.
         server_ip=$(ping -4 -c 1 ${SERVER_HOST} | head -n 1 | cut -d ' ' -f 3 | cut -d '(' -f 2 | cut -d ')' -f 1)
         server="${server_ip}#${SERVER_PORT}"
         echo "Upstream server: ${server}"
