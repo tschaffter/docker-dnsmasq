691505eab4a1a82ae612f7c21d53c87a45e8279c
tschaffter <thomas.schaffter@gmail.com>
2021-03-24 20:09:40 -0700

    Can now pass multiple dynamic nameservers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,39 @@
 set -e
 
 if [ "$1" = 'dnsmasq' ]; then
-    if [[ ! -z "${SERVER_HOST}" && ! -z "${SERVER_PORT}" ]]; then
-        # Lookup IP of nameserver container as work around because dnsmasq
-        # cannot resolve the address from the nameserver "server". This uses
-        # ping rather than 'dig +short server' to avoid needing dnsutils
-        # package.
-        server_ip=$(ping -4 -c 1 ${SERVER_HOST} | head -n 1 | cut -d ' ' -f 3 | cut -d '(' -f 2 | cut -d ')' -f 1)
-        server="${server_ip}#${SERVER_PORT}"
-        echo "Upstream server: ${server}"
-        set -- "$@" "--server=${server}"
-    fi
+    command=("$1")
+    shift
+
+    for arg in "$@"
+    do
+        case $arg in
+            -S|--local|--server)
+            # Split the server specified into host and port
+            IFS='#' read -ra items <<< "$2"
+            # Lookup IP of nameserver container as work around because dnsmasq
+            # cannot resolve the address from the nameserver "server". This uses
+            # ping rather than 'dig +short server' to avoid needing dnsutils
+            # package.
+            server=$(ping -4 -c 1 ${items[0]} | head -n 1 | cut -d ' ' -f 3 | cut -d '(' -f 2 | cut -d ')' -f 1)
+            if [ ${#items[@]} -ge 1 ]; then
+                server+="#${items[1]}"
+            fi
+            command+=("$1" "$server")
+            shift  # Remove argument name from processing
+            shift  # Remove argument value from processing
+            ;;
+            *)
+            if [[ ! -z "$1" ]]; then
+                command+=("$1")
+                shift  # Remove generic argument from processing
+            fi
+            ;;
+        esac
+    done
+
     echo "$(dnsmasq --version)"
-    echo "$@"
-    exec gosu dnsmasq "$@"
+    echo "${command[@]}"
+    exec gosu dnsmasq "${command[@]}"
 fi
 
 exec "$@"
